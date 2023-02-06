a8d84d077fcf481a1a8e82aaeedc5f62063fa5cc
Joe <josephfmck@gmail.com>
2023-02-05 20:38:37 -0600

    brought in react bootstrap, transferred Home page css

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,7 @@
 //* bootstrap components 
 import Container from 'react-bootstrap/Container';
-import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
 
 
 import './index.scss';
@@ -10,11 +11,53 @@ import './index.scss';
 //add Routes, start with path "/" and <Layout/> with <Home/> 
 function Home() {
   return (
-    <Container className="p-3">
-      <Card>
-      <h1 className="header">Welcome To React-Bootstrap</h1>
-      </Card>
+    <>
+    <Container id="header">
+      <h1 className="text-center"><strong>NATIONAL PARKS</strong></h1>
+      <h3 className="text-center">TRAVEL GUIDE</h3>
     </Container>
+    
+    {/* convert #search-section to a component */}
+    <section id="search-section">
+      <Container>
+        <Row>
+          <Form id="search-form">
+
+            {/*Directly takes to park page on click */}
+            <Form.Group>
+              <Form.Label className="search-label">Pick a National Park</Form.Label>
+              <Form.Select className="search-selectTag mb-5" aria-label="Select A State">
+                <option>Search for a national park</option>
+                <option value="Acadia National Park">Acadia National Park</option>
+                <option value="IL">IL</option>
+                <option value="WA">WA</option>
+              </Form.Select>
+            </Form.Group>
+
+            {/* Brings up component list of all parks within state */}
+            <Form.Group>
+            <Form.Label className="search-label">Search a Park By State</Form.Label>
+              <Form.Select className="search-selectTag mb-5" aria-label="Select A Park">
+                <option>Select a State</option>
+                <option value="KS">Kansas</option>
+                <option value="IL">Illinois</option>
+                <option value="WA">Washington</option>
+              </Form.Select>
+            </Form.Group>
+
+            {/* REPLACED WITH ONCLICK ON THE OPTIONS ABOVE */}
+            {/* <button
+              className="btn btn-light btn-lg search-btn"
+              id="select-state-btn"
+            >
+              SELECT PARK
+            </button> */}
+
+          </Form>
+        </Row>
+      </Container>
+    </section>
+    </>
   );
 }
 
