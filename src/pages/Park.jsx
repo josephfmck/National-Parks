fb6f56225439531ffdf1c8c5a2ae16565a66e105
Joe <josephfmck@gmail.com>
2023-04-10 15:05:12 -0500

    styled park page

--- src/pages/Park.jsx
+++ src/pages/Park.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 
 //*redux
 import {useDispatch, useSelector} from 'react-redux'
@@ -42,13 +43,30 @@ function Park() {
         <Container id="header" className="">
             <h1 className='text-center'><strong>{parkApiData.data[0].fullName}</strong></h1>
         </Container>
-        <Container id="container">
-            <h3>{parkApiData.data[0].description}</h3>
-            <h1>Directions:</h1>
-            <h3>{parkApiData.data[0].directionsInfo}</h3>
-            
-            <h3>Operating Hours</h3>
-            <h3>{parkApiData.data[0].operatingHours[0].description}</h3>            
+        <Container id="info-container">
+            <div className='info p-3 m-5'>
+                <p>{parkApiData.data[0].description}</p> 
+                <p>{parkApiData.data[0].directionsInfo}</p>     
+            </div>
+            <Row>
+                {parkApiData.data[0].images.map((img, index) => {
+                    return (
+                    <Col className="my-3" key={index}>
+                        <Card style={{ width: '18rem', height:'100%' }}>
+                            <Card.Img variant="top" className="preview-img" src={img.url} />
+                            <Card.Body>
+                                <Card.Title>{img.title}</Card.Title>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    )
+                })}
+            </Row>
+            <div className='info p-4 my-5'>
+                <h1>Operating Hours</h1>
+                <h3>{parkApiData.data[0].operatingHours[0].description}</h3>            
+            </div>
+            <div className='info p-4 my-5'>
                 <h1>Address:</h1>
                     <Row>
                         <h3>{parkApiData.data[0].addresses[0].line1}</h3>
@@ -60,13 +78,18 @@ function Park() {
                             {parkApiData.data[0].addresses[0].postalCode}
                         </h3>
                     </Row>
-            <h1>Contact Info</h1>
-            <h3>{parkApiData.data[0].contacts.phoneNumbers[0].phoneNumber}</h3>
-            <h3>{parkApiData.data[0].contacts.emailAddresses[0].emailAddress}</h3>
-            <h3>etc.</h3>
-            <h3>{parkApiData.data[0].parkCode}</h3>
-            <h3>{parkApiData.data[0].directionsUrl}</h3>
-            <h3>{parkApiData.data[0].url}</h3>
+            </div>
+            <div className="info p-4 my-5">
+                <h1>Contact Info</h1>
+                <h3>{parkApiData.data[0].contacts.phoneNumbers[0].phoneNumber}</h3>
+                <h3>{parkApiData.data[0].contacts.emailAddresses[0].emailAddress}</h3>
+            </div>
+            <div className="info p-4 my-5">
+                <h3>etc.</h3>
+                <h3>{parkApiData.data[0].parkCode}</h3>
+                <h3>{parkApiData.data[0].directionsUrl}</h3>
+                <h3>{parkApiData.data[0].url}</h3>
+            </div>
         </Container>
         </div>
   )
